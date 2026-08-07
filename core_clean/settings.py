e2bcf74b1804ab4bdb5dc73045a2c60cee42042e
julician <juli.izin07@gmail.com>
2026-08-07 17:49:52 -0300

    Agregar recuperacion de contraseña

--- core_clean/settings.py
+++ core_clean/settings.py
@@ -209,3 +209,8 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
 ]
+# =====================================================
+# EMAIL - DESARROLLO LOCAL
+# =====================================================
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
