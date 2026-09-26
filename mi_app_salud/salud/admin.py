6c0bfbe4632b04bccee07969850ff1cca63877c3
julician <juli.izin07@gmail.com>
2026-09-25 21:50:52 -0300

    Ordenar estructura del repositorio

--- mi_app_salud/salud/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Paciente, RegistroSalud
-
-admin.site.register(Paciente)
-admin.site.register(RegistroSalud)
--- mi_app_salud/salud/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SaludConfig(AppConfig):
-    name = 'salud'
